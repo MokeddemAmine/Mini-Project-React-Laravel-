f3fabfc97dbeb5d62a461b78ab9642b23c2aebbf
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-10-07 17:43:40 +0200

    contextAPI with protection for Auth

--- react/src/components/GuestLayouts.jsx
+++ react/src/components/GuestLayouts.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../contexts/ContextProvider'
 
 export default function GuestLayouts() {
+    const {token} = useStateContext();
+    if(token){
+        return <Navigate to="/"/>
+    }
   return (
     <div>
         
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -4,9 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
+import { ContextProvider } from './contexts/ContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <ContextProvider>
+      <RouterProvider router={router}/>
+    </ContextProvider>
+    
   </StrictMode>,
 )
