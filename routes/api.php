78c20979229c6a7ef4080a9fbdda61103ab8ce43
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-10-07 19:49:53 +0200

    backend request and response for auth Users

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::controller(AuthController::class)->group(function(){
+    Route::post('/signup','signup');
+    Route::post('/login','login');
+    Route::post('/logout','logout');
+});
