07b4c4ad516725171a2e1f5d2509c13ecd065326
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-10-07 18:05:17 +0200

    design login,signup and defaultLayout

--- react/src/views/SignUp.jsx
+++ react/src/views/SignUp.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
   return (
     <div>
-      SignUp
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h3 className="title">Sign up for free</h3>
+          <input type='text' placeholder='Full Name'/>
+          <input type='email' placeholder='Email'/>
+          <input type='password' placeholder='Password'/>
+          <input type='password' placeholder='Password Confirmation'/>
+          <button className="btn btn-block">Sign UP</button>
+          <p className="message">
+            Already Registered? <Link to="/login">Sign in</Link>
+          </p>
+        </form>
+      </div>
     </div>
+  </div>
   )
 }
